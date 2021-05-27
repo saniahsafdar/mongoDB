580c4107b0f61c9928608edf681dc28325d677b2
Saniah Safdar <77857761+saniahsafdar@users.noreply.github.com>
2021-05-27 00:02:27 -0400

    Add files via upload

---
+++ mongodb2.js
@@ -0,0 +1,31 @@
+// Add a person to the collection. You pick the data, but they should have an empty array for children.
+db.people.insertOne({ first_name: "Mary", last_name: "Smith", email: "maryu@gmail.edu", gender:"Female", age: 17, state: "Ohio", "children": [] })
+
+// Add another person. They should have at least two children.
+
+// Update one person named Clarence. He moved from North Dakota to South Dakota.
+
+// Update Rebecca Hayes. Remove her email address.
+
+// Update everyone from Missouri. They all had a birthday today, so add one to their age. (expect 4 matches)
+
+// Jerry Baker has updated information. Replace with a new document:
+// { first_name: "Jerry", last_name: "Baker-Mendez", email: "jerry@classic.ly", gender:"Male", age: 28, state: "Vermont", "children": [{name: "Alan", age: 18}, {name: "Jenny", age: 3}] }
+// Delete Wanda Bowman.
+// Delete everyone who does not have an email address specified. (expect 37 matches)
+
+// In submissions collection
+// Add several documents to a new submissions collection. Do it all in one command. (Remember, MongoDB will create the collection for you. Just start adding documents.)
+// title: "The River Bend", upvotes: 10, downvotes: 2, artist: <ID of Anna Howard>
+// title: "Nine Lives", upvotes: 7, downvotes: 0, artist: <ID of Scott Henderson>
+// title: "Star Bright", upvotes: 19, downvotes: 3, artist: <ID of Andrea Burke>
+// title: "Why Like This?", upvotes: 1, downvotes: 5, artist: <ID of Steven Marshall>
+// title: "Non Sequitur", upvotes: 11, downvotes: 1, artist: <ID of Gerald Bailey>
+// Add 2 upvotes for "The River Bend".
+// Add a field round2 = true to all submissions with at least 10 upvotes. (expect 3 matches)
+
+// Extended Challenges:
+// Update Helen Clark. She had a baby! Add a child, name: Melanie, age: 0.
+// Joan Bishop has a child named Catherine. She just had a birthday and prefers to go by "Cat". In one query update the child's name to "Cat" and increment her age by one.
+// List all submissions that have more downvotes than upvotes.
+

7ac17d3c01da3274f5b91db8d1c76fb89a979f5e
Saniah Safdar <77857761+saniahsafdar@users.noreply.github.com>
2021-05-27 10:09:09 -0400

    Update aggregation.js

--- aggregation.js
+++ aggregation.js
@@ -1,1 +1,11 @@
-test 
+db.menu.insertMany([
+ { "name": "Brewed Coffee", "price": 2.00, "type": "Coffee" },
+ { "name": "Iced Coffee",   "price": 2.50, "type": "Coffee" },
+ { "name": "Cafe au Lait",  "price": 2.25, "type": "Coffee" },
+ { "name": "Espresso",      "price": 2.50, "type": "Espresso" },
+ { "name": "Latte",         "price": 3.00, "type": "Espresso" },
+ { "name": "Mocha Latte",   "price": 3.50, "type": "Espresso" },
+ { "name": "Tea",           "price": 2.00, "type": "Tea" },
+ { "name": "Chai",          "price": 3.50, "type": "Tea" },
+ { "name": "Hot Chocolate", "price": 3.00, "type": "Other" }
+]);
